6cb64c110dcb1a207a6f1faf2330ec16dc3d2315
JPico6 <JPico6@users.noreply.github.com>
2016-08-08 18:03:14 -0400

    Update mixed effects models.R

--- mixed effects models.R
+++ mixed effects models.R
@@ -1,12 +1,13 @@
 
+################################################################################
+##########Tests of relationship length on self and partner enhancement##########
 
-###############Tests of relationship length on self and partner enhancement
 library(lme4)
 library(lmerTest)
 library(reshape2)
 library(ggplot2)
 
-#create difference score
+##########create difference score##########
 df <- read.csv("Reorganized1.csv", header=TRUE)
 
 df$discrF <- df$Foverall.2 - df$Foverall.3
@@ -27,7 +28,6 @@ ggplot(df2, aes(x=reLength, y=value)) +
   geom_point(shape=1) +    # Use hollow circles
   geom_smooth()            # Add a loess smoothed fit curve with confidence region
 
-
 ML1 <- lmer(value~reLength + (1 | couple),data=df2)
 summary(ML1)
 
@@ -40,12 +40,7 @@ MLnull <- lmer(value~ + (1 | couple), data=df2, REML=FALSE)
 anova(MLnull,MLhyp)   #gives us the chi 2 comparison of null and hyp models
 anova(MLnull,MLhyp2)   #MLhyp works better
 
-
-
-
-
-
-#calculate r2
+##########calculate r2##########
 cor(ML1$value, fitted(lmer(value~reLength + (1 | couple),data=df2)))^2
 
 cor(l$RT, fitted(lmer(RT ~ cNativeEnglish * cFrequency + Trial +
@@ -56,7 +51,6 @@ anova(ML1,ML2)
 #help("pvalues")     #for help in getting p values
 #the model was fitted by restricted maximum likelihood (REML)
 
-
 plot(ML1,type=c("p","smooth")) ## fitted vs residual
 plot(ML1,sqrt(abs(resid(.)))~fitted(.), ## scale-location
      type=c("p","smooth"))
@@ -65,8 +59,6 @@ qqmath(ML1,id=0.05)
 iqrvec <- sapply(simulate(ML1,1000),IQR)
 obsval <- IQR(df2$value)
 post.pred.p <- mean(obsval>=c(obsval,iqrvec))
-
-
 
 lsmeans(ML1, test.effs="reLength")
 plot(ML1)
